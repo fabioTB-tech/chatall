c5cba8bf5b1d712bcb12ca6fccfdc83119ffd47c
fabioTB-tech <drawingfabio@gmail.com>
2026-05-04 15:03:48 -0300

    Conexão com banco de dados

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,18 +81,10 @@ WSGI_APPLICATION = 'chat_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        }
-    }
+    'default': dj_database_url.config(
+        default='postgresql://chatall_db_user:qPBBCzkEiWrKMWXpoZyxfglGgzM71io9@dpg-d7sd80e7r5hc739bpkdg-a.oregon-postgres.render.com/chatall_db',
+        conn_max_age=600
+    )
 }
 
 
