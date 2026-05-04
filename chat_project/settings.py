a7d820a0e3fc06ef45552bccaa59871a20e0c3e2
fabioTB-tech <drawingfabio@gmail.com>
2026-05-04 14:25:21 -0300

    usando PostgreSQL

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'chat_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'),
